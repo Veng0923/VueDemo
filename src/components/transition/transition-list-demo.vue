9f0be14a3f6faac4457f9025df4025bd440895f7
Veng <1216116203@qq.com>
2019-09-19 15:09:06 +0800

    数字移动动画

--- src/components/transition/transition-list-demo.vue
+++ src/components/transition/transition-list-demo.vue
@@ -1,26 +1,33 @@
 
 <template>
     <div>
-        <button @click="add">add</button>
-        <button @click="remove">remove</button>
+        <hr/>
+        <el-button size="mini" @click="add">add</el-button>
+        <el-button size="mini" @click="remove">remove</el-button>
+        <el-button size="mini" @click="shuffle" plain>shuffle</el-button>
         <br>
             <transition-group name="fade" tag="ul">
                     <li v-for="item in items" :key="item">
                         <span>{{item}}</span>
                     </li>
             </transition-group>
+        <transition-group name="fade-shuffle" tag="ul">
+            <li v-for="item in items" :key="item">
+                <span>{{item}}</span>
+            </li>
+        </transition-group>
     </div>
 </template>
 
 <script>
-    import {test} from '../util';
-
     export default {
         name: "transition-list-demo",
-        props:['items'],
+        props:{
+            items:Array,
+        },
         methods:{
             getRandom: function () {
-                return test();
+                return Math.random()*this.items.length;
             },
             add(){
                 this.items.splice(this.getRandom(),0,Math.max(...this.items)+1);
@@ -28,10 +35,18 @@
             remove(){
                 this.items.splice(this.getRandom(),1);
             },
+            shuffle(){
+                this.items.sort(()=>{
+                    return Math.random()-0.5;
+                });
+            },
         },
     }
 </script>
 
 <style scoped>
     @import "../fade.css";
+    .fade-shuffle-move{
+        transition: transform 1s;
+    }
 </style>
