79748a2280eb4ba38c02dde2ba68f63565dcc1c2
Veng <1216116203@qq.com>
2019-09-20 17:56:20 +0800

    颜色增变未完成，半成

---
+++ src/components/transition/transition-color-demo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <hr>
+        <el-input @keydown.enter="updateColor" type="text" v-model="inputColor" placeholder="please input color"
+                  class="input"></el-input>
+        <el-button @click="updateColor">update</el-button>
+        <div class="color-div" :style="{backgroundColor:currentColor}">
+
+        </div>
+        <p>
+            {{currentColor}}
+        </p>
+    </div>
+</template>
+
+<script>
+    import Color from 'color';
+    export default {
+        name: "transition-color-demo",
+        data:function () {
+            return {
+                backgroundColor:'#000000',
+                inputColor:'',
+            }
+        },
+        computed:{
+            rgbBackgroundColor:function () {
+                return new Color(this.backgroundColor).toRgb();
+            }
+        },
+        methods:{
+            updateColor(){
+
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
